0c79c0b99c4ed0d8dd0a28421c01f55b78210467
Adrian-Aguilera <110703102+Adrian-Aguilera@users.noreply.github.com>
2024-06-14 14:44:21 -0600

    fix: diseño input  validation error

--- App/js/Validations.js
+++ App/js/Validations.js
@@ -1,11 +1,14 @@
 function validarDUI(dui) {
+    const duiInput = document.getElementById('dui');
     const regex = /^\d{9}$/;
-    
+
     if (regex.test(dui)) {
-        console.log("valido");
+        console.log("Válido");
+        duiInput.classList.remove('is-invalid');  // Suponiendo que 'is-invalid' es una clase para mostrar error
         return true;
     } else {
-        alert("El formato del DUI no es válido. Asegúrese de que tenga el formato 00000000-0.");
+        duiInput.classList.add('is-invalid');  // Agrega una clase para cambiar el estilo en caso de error
         return false;
     }
 }
+
